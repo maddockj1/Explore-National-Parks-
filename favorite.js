01341c98b690d40824be5f73c88b1a6af1ff23c6
maddockj1 <jmmaddock@gmail.com>
2018-09-19 08:27:16 -0600

    wed morning

---
+++ favorite.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function() {
+  console.log(DOMContentLoaded)
+function favoriteDivs(){
+  //create a div for the favorite park to go in
+  let favoriteTitleDiv=document.createElement("div")
+//add a class for alignment
+  favoriteTitleDiv.classList.add("center-align")
+//append to container
+document.getElementById()
+}
+function displayFavs(){
+
+}
+//DOM Content Loaded bracket
+})
--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-
 document.addEventListener('DOMContentLoaded', function() {
   $(document).ready(function() {
     $('select').formSelect();
@@ -13,11 +12,11 @@ document.addEventListener('DOMContentLoaded', function() {
     //allows URL to be changed by drop menu
     let url = `https://api.nps.gov/api/v1/parks?api_key=v8ZxkynxpNzIjrYpuGL7Vr6trBu2WzWT9CSPaowj&fields=images&parkCode=${stateSelect}`
     //console.log(url)
-     let stateData
+    let stateData
     //       //axios.get(url)
     //       //.then((response) => {
-           stateData = COLORADO["data"]
-           console.log(stateData)
+    stateData = COLORADO["data"]
+    console.log(stateData)
     //     })
     // }
     //function to create proper divs
@@ -25,20 +24,20 @@ document.addEventListener('DOMContentLoaded', function() {
       for (let i = 0; i < stateData.length; i++) {
 
         //create row for title
-        let titleDiv= document.createElement("div")
+        let titleDiv = document.createElement("div")
         titleDiv.classList.add("row")
-        titleDiv.id="title"
+        titleDiv.id = "title"
         titleDiv.classList.add("title1")
 
         //create an element for title to go in
-        let parkTitle= document.createElement("h3")
+        let parkTitle = document.createElement("h3")
         //add a class to adust the title into the center
         parkTitle.classList.add("center-align")
 
-//create row div for img and text
-        let alignImgText=document.createElement("div")
+        //create row div for img and text
+        let alignImgText = document.createElement("div")
         alignImgText.classList.add("row")
-        alignImgText.id="alignment"
+        alignImgText.id = "alignment"
 
         let imgTag = document.createElement("img")
         //add a class to img div for sizing
@@ -52,18 +51,18 @@ document.addEventListener('DOMContentLoaded', function() {
         textDiv.classList.add("s8")
 
         //add a div row for button
-        let buttonDiv=document.createElement("div")
+        let buttonDiv = document.createElement("div")
         //add row class to div
         buttonDiv.classList.add("row")
-        buttonDiv.id="button"
+        buttonDiv.id = "button"
         //add a button to Favorite a park
-        let favButton= document.createElement("a")
+        let favButton = document.createElement("a")
         favButton.classList.add("waves-effect")
         favButton.classList.add("waves-light")
         favButton.classList.add("btn")
         favButton.classList.add("green")
         favButton.classList.add("darken-4")
-
+        favButton.classList.add("btn1")
         //append divs to results div so they appear before selector screen
         //appending title row to container
         document.getElementById('contain').appendChild(titleDiv)
@@ -83,23 +82,42 @@ document.addEventListener('DOMContentLoaded', function() {
 
         //append button to its row
         buttonDiv.appendChild(favButton)
+
       }
     }
     createDivLayout()
     //function to get data needed from the api
     //let parkImg
-  //loop to get info for page
+    //loop to get info for page
     function parkData() {
       for (i = 0; i < stateData.length; i++) {
-        document.getElementsByClassName("center-align")[i].innerText=stateData[i].fullName
-        document.getElementsByClassName("s4")[i].src = stateData[i].images[0].url
+        document.getElementsByClassName("center-align")[i].innerText = stateData[i].fullName
+        document.getElementsByClassName("s4")[i + 1].src = stateData[i].images[0].url
         document.getElementsByClassName("s8")[i].innerText = stateData[i].description
-        document.getElementsByClassName("btn")[i+1].innerText= "Favorite!"
+        document.getElementsByClassName("btn1")[i].innerText = "Favorite!"
       }
     }
     parkData()
+    console.log(document.getElementsByClassName("btn1"))
 
+
+    //eventlistener function for all fav buttons
+    function addFavorites() {
+      let favoriteButtons = document.getElementsByClassName("btn1")
+      console.log(favoriteButtons);
+      for (let i = 0; i < favoriteButtons.length; i++) {
+        favoriteButtons[i].addEventListener('click', function() {
+          event.preventDefault()
+          //array of park names
+          let nameArr = document.getElementsByClassName("center-align")
+          console.log(nameArr[i])
+          //loop through buttons and names hopefully
+          localStorage.setItem("park", nameArr[i].innerText)
+        })
+      }
+    }
+    addFavorites()
+    //end brackets of submit
   })
-  //
   //end bracket DOM content loaded
 })
