fccb600a71d7488b079729a9ab7c3f305c0e8ff2
maddockj1 <jmmaddock@gmail.com>
2018-09-19 14:35:13 -0600

    minimal acceptable requirements

--- favorite.js
+++ favorite.js
@@ -1,15 +1,28 @@
-document.addEventListener('DOMContentLoaded', function() {
-  console.log(DOMContentLoaded)
-function favoriteDivs(){
-  //create a div for the favorite park to go in
-  let favoriteTitleDiv=document.createElement("div")
-//add a class for alignment
-  favoriteTitleDiv.classList.add("center-align")
-//append to container
-document.getElementById()
-}
-function displayFavs(){
 
-}
-//DOM Content Loaded bracket
+function favoriteDivs() {
+    //create a div for the favorite park to go in
+    let favoriteTitleDiv = document.createElement("div")
+    //add a class for alignment
+    favoriteTitleDiv.classList.add("center-align")
+    //append to container
+    document.getElementById('contain').appendChild(favoriteTitleDiv)
+    //create a button
+  }
+  let favorite= localStorage.getItem("park")
+  console.log(favorite)
+  //write a function to check local storage and display based on what's there
+  function displayFavs() {
+    if(!localStorage.getItem("park")){
+    document.getElementsByClassName('center-align')[0].innerText="What are you doing?!? Choose a favorite park!"
+  }else {
+    document.getElementsByClassName('center-align')[0].innerText=`Your Favorite Park is ${favorite}`
+  }
+  }
+document.addEventListener('DOMContentLoaded', function() {
+  console.log("DOMContentLoaded")
+
+favoriteDivs()
+displayFavs()
+
+  //DOM Content Loaded bracket
 })
--- test/tests.js
+++ test/tests.js
@@ -1,1 +1,25 @@
 const expect = chai.expect
+
+describe("check tests are running",()=>{
+  it("ran a test", ()=>{
+    expect(true).to.equal(true);
+  })
+})
+
+
+describe("favoriteDivs should be a function",()=>{
+  it("is a function", ()=>{
+    expect(favoriteDivs).to.be.a('function')
+  })
+})
+
+describe("displayFavs should be a function",()=>{
+  it ("is a funciton", ()=>{
+    expect(displayFavs).to.be.a('function')
+  })
+})
+describe("check local storage",()=>{
+  it("is empty", ()=>{
+    expect(displayFavs()).to.equal(false)
+  })
+})
